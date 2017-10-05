216654f7772b76622bcc7548b69e624be7b8aa16
John Franco Saraceno <jfsaraceno@gmail.com>
2017-10-05 10:02:15 -0700

    Update documentation

--- setup.py
+++ setup.py
@@ -9,19 +9,16 @@ with open('README.RST') as f:
 
 with open('LICENSE') as f:
     license = f.read()
-url = 'https://my.usgs.gov/bitbucket/projects/BGCTECH/repos/burpro/browse''
+url = 'https://my.usgs.gov/bitbucket/projects/BGCTECH/repos/burpro/browse'
 setup(name='burpro',
-      version='1.0',
-      description='burpro module',
+      version='1.1',
+      description='A module to process burst XYLEM EXO .xls data files',
       long_description=readme,
-      platforms=['Microsoft :: Windows :: Windows 7'],
       author='John Franco Saraceno',
       author_email='saraceno@usgs.gov',
       url=url,
       download_url=url,
-      install_requires=['numpy', 'pandas', 'statsmodels', 'validictory'],
-      scripts=[],
-      Development=['Development Status :: 4 - Beta'],
+      install_requires=['numpy', 'pandas', 'statsmodels'],
       license=license,
       packages=pkgs,
       include_package_data=True,
